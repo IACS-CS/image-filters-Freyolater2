48e25058dbb6f92cff1b95f781e90b2e52577c2c
Alex Frey <147182008+Freyolater2@users.noreply.github.com>
2024-01-24 13:34:19 +0000

    added filters to the index

--- src/filters/index.tsx
+++ src/filters/index.tsx
@@ -2,10 +2,15 @@ import type { Filter } from "../types";
 import sampleRoseColoredGlasses from "./samples/roseColoredGlasses";
 import sampleGrid from "./samples/grid";
 import sampleVignette from "./samples/vignette";
+import { Brightness } from "./brightness";
+import {Blueness} from "./blueness";
 const filters: Filter[] = [
+  Brightness,
+  Blueness,
   sampleRoseColoredGlasses,
   sampleGrid,
   sampleVignette,
 ];
 
 export default filters;
+
