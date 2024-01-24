9ae0f18940db3347f180b3c35d2a253add9c737f
Alex Frey <147182008+Freyolater2@users.noreply.github.com>
2024-01-24 13:52:11 +0000

    work in progress

--- src/filters/border.tsx
+++ src/filters/border.tsx
@@ -1,28 +1,29 @@
 import type { Filter } from "../types";
 import { hexToRGBA } from "../utils";
 
-type MyFilterOptions = {
+type BorderFilterOptions = {
   /* Define options here, as we will get them
   in our apply function -- this needs to match
   the list of options provided below. I'll provide
   three example options to show one of each type */
   color: string;
-  strength: number;
-  darkMode: boolean;
+  size: number;
 };
 
-export const Border: Filter<MyFilterOptions> = {
+export const Border: Filter<BorderFilterOptions> = {
   name: "Border",
   apply: (pixels, width, height, options) => {
     // We can now access our options with
     // e.g. options.color, options.strength, etc.
-    
-
+    let getIndices = getIndexer(pixels,width,height)
+    for (let row = 0; row < height; row++) {
+      for (let col = 0; col < width; col++) {
+        const indices = getIndices(row,col);
+        let redValue 
     // Note that "color" options give you a string (e.g. a hex code)
     // and our utils library has a convenience function to convert
     // from RGB (or RGBA) hex codes into an array of numbers (0-255)
     const [r, g, b, a] = hexToRGBA(options.color);
-    /* Modify pixels... */
     return pixels;
   },
   options: [
@@ -32,16 +33,11 @@ export const Border: Filter<MyFilterOptions> = {
       default: "#ff0000",
     },
     {
-      name: "strength",
-      type: "number",
-      default: 50,
-      min: 0,
-      max: 1000,
-    },
-    {
-      name: "darkMode",
-      type: "boolean",
-      default: false,
+      name: "size",
+      type: "percentage",
+      default: 20,
+      min: .01,
+      max: 100,
     },
   ],
 };
