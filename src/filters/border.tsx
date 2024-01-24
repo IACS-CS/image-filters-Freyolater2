5f9e9b09a3766f3a548b30211002c5b225ca7ed0
Alex Frey <147182008+Freyolater2@users.noreply.github.com>
2024-01-24 14:29:51 +0000

    fixed problems

--- src/filters/border.tsx
+++ src/filters/border.tsx
@@ -15,38 +15,41 @@ type BorderFilterOptions = {
 export const Border: Filter<BorderFilterOptions> = {
   name: "Border",
   apply: (pixels,width,height,options) => {
+    const [r, g, b, a] = hexToRGBA(options.color);
     let getIndices = getIndexer(pixels,width,height)
     for (let row = 0; row < height; row++) {
       for (let col = 0; col < width; col++) {
         const indices = getIndices(row,col);
-        let redValue = pixels[indices[R]]
+        let redValue = pixels[indices[R]];
       const borderwidth = width * 0.05
       if (row < borderwidth) {
-        pixels[indices[R]] = 0;
-        pixels[indices[G]] = 0;
-        pixels[indices[B]] = 0;
+        pixels[indices[R]] = r;
+        pixels[indices[G]] = g;
+        pixels[indices[B]] = b;
       }
       if (col < borderwidth) {
-        pixels[indices[R]] = 0;
-        pixels[indices[G]] = 0;
-        pixels[indices[B]] = 0;
+        pixels[indices[R]] = r;
+        pixels[indices[G]] = g;
+        pixels[indices[B]] = b;
       }
 
       if (row > height - borderwidth) {
-        pixels[indices[R]] = 0;
-        pixels[indices[G]] = 0;
-        pixels[indices[B]] = 0;
+        pixels[indices[R]] = r;
+        pixels[indices[G]] = g;
+        pixels[indices[B]] = b;
       }
 
       if (col > width - borderwidth) {
-        pixels[indices[R]] = 0;
-        pixels[indices[G]] = 0;
-        pixels[indices[B]] = 0;
+        pixels[indices[R]] = r;
+        pixels[indices[G]] = g;
+        pixels[indices[B]] = b;
       }
+    }
+  }
     // Note that "color" options give you a string (e.g. a hex code)
     // and our utils library has a convenience function to convert
     // from RGB (or RGBA) hex codes into an array of numbers (0-255)
-    const [r, g, b, a] = hexToRGBA(options.color);
+    
     return pixels;
   },
   options: [
@@ -59,8 +62,7 @@ export const Border: Filter<BorderFilterOptions> = {
       name: "size",
       type: "percentage",
       default: 20,
-      min: .01,
-      max: 100,
+
     },
   ],
 };
