4a78de72f6af397d8361f159e6b15f339637be57
Alex Frey <147182008+Freyolater2@users.noreply.github.com>
2024-01-24 13:51:55 +0000

    added border filter

--- src/filters/index.tsx
+++ src/filters/index.tsx
@@ -2,11 +2,13 @@ import type { Filter } from "../types";
 import sampleRoseColoredGlasses from "./samples/roseColoredGlasses";
 import sampleGrid from "./samples/grid";
 import sampleVignette from "./samples/vignette";
-import { Brightness } from "./brightness";
+import {Brightness} from "./brightness";
 import {Blueness} from "./blueness";
+import {Border} from "../border";
 const filters: Filter[] = [
   Brightness,
   Blueness,
+  Border,
   sampleRoseColoredGlasses,
   sampleGrid,
   sampleVignette,
