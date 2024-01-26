022c1dc946682a6e9c09d03ca06cd997d81c0c91
Alex Frey <147182008+Freyolater2@users.noreply.github.com>
2024-01-26 16:24:54 +0000

    added size and color options

--- src/filters/border.tsx
+++ src/filters/border.tsx
@@ -2,10 +2,10 @@ import type { Filter } from "../types";
 import { hexToRGBA } from "../utils";
 import { getIndexer } from "../utils";
 
-const R = 0
-const G = 1
-const B = 2
-const A = 3
+const R = 0;
+const G = 1;
+const B = 2;
+const A = 3;
 
 type BorderFilterOptions = {
   color: string;
@@ -14,42 +14,44 @@ type BorderFilterOptions = {
 
 export const Border: Filter<BorderFilterOptions> = {
   name: "Border",
-  apply: (pixels,width,height,options) => {
+  apply: (pixels, width, height, options) => {
     const [r, g, b, a] = hexToRGBA(options.color);
-    let getIndices = getIndexer(pixels,width,height)
+    const borderwidth = width * Number(options.size)/100;
+    console.log("Got options", options);
+    let getIndices = getIndexer(pixels, width, height);
     for (let row = 0; row < height; row++) {
       for (let col = 0; col < width; col++) {
-        const indices = getIndices(row,col);
+        const indices = getIndices(row, col);
         let redValue = pixels[indices[R]];
-      const borderwidth = width * 0.05
-      if (row < borderwidth) {
-        pixels[indices[R]] = r;
-        pixels[indices[G]] = g;
-        pixels[indices[B]] = b;
-      }
-      if (col < borderwidth) {
-        pixels[indices[R]] = r;
-        pixels[indices[G]] = g;
-        pixels[indices[B]] = b;
-      }
 
-      if (row > height - borderwidth) {
-        pixels[indices[R]] = r;
-        pixels[indices[G]] = g;
-        pixels[indices[B]] = b;
-      }
+        if (row < borderwidth) {
+          pixels[indices[R]] = r;
+          pixels[indices[G]] = g;
+          pixels[indices[B]] = b;
+        }
+        if (col < borderwidth) {
+          pixels[indices[R]] = r;
+          pixels[indices[G]] = g;
+          pixels[indices[B]] = b;
+        }
 
-      if (col > width - borderwidth) {
-        pixels[indices[R]] = r;
-        pixels[indices[G]] = g;
-        pixels[indices[B]] = b;
+        if (row > height - borderwidth) {
+          pixels[indices[R]] = r;
+          pixels[indices[G]] = g;
+          pixels[indices[B]] = b;
+        }
+
+        if (col > width - borderwidth) {
+          pixels[indices[R]] = r;
+          pixels[indices[G]] = g;
+          pixels[indices[B]] = b;
+        }
       }
     }
-  }
     // Note that "color" options give you a string (e.g. a hex code)
     // and our utils library has a convenience function to convert
     // from RGB (or RGBA) hex codes into an array of numbers (0-255)
-    
+
     return pixels;
   },
   options: [
@@ -62,7 +64,8 @@ export const Border: Filter<BorderFilterOptions> = {
       name: "size",
       type: "percentage",
       default: 20,
-
+      min: 1,
+      max : 50,
     },
   ],
 };
